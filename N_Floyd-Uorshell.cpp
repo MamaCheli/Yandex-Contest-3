ce7cf2e4ec9f8e059e83b378135ecbc23b81b894
MamaCheli <lopatinartem1@gmail.com>
2021-12-13 02:19:52 +0300

    N

--- N_Floyd-Uorshell.cpp
+++ N_Floyd-Uorshell.cpp
@@ -1,43 +1,87 @@
 #include <iostream>
 #include <vector>
 
-using Vertex = int64_t;
-using DistT = int64_t;
+using VERTEX = int64_t;
+using DIST = int64_t;
 
-class Graph {
-    Vertex num_vertex_;
-    std::vector<std::vector<DistT>> matrix_;
+class GraphList {
+public:
+    VERTEX num_vertex_;
+    VERTEX num_edge_;
+    struct Edge {
+        VERTEX from;
+        VERTEX to;
+        DIST weight;
+    };
+    std::vector<Edge> list_;
+
+    GraphList(VERTEX, VERTEX);
+    void AddEdge(VERTEX, VERTEX, DIST);
+};
+
+GraphList::GraphList(VERTEX num_vertex, VERTEX num_edge) {
+    num_vertex_ = num_vertex;
+    num_edge_ = num_edge;
+    list_.resize(num_edge_);
+}
+
+void GraphList::AddEdge(VERTEX u, VERTEX v, DIST weight) {
+    if (u == v) {
+        return;
+    }
+    Edge add_e;
+    add_e.from = u;
+    add_e.to = v;
+    add_e.weight = weight;
+    list_.push_back(add_e);
+}
+
+class GraphMatrix {
+    VERTEX num_vertex_;
+    std::vector<std::vector<DIST>> matrix_;
     void FloydWarshall();
 
 public:
-    explicit Graph(Vertex);
-    void AddEdge(Vertex, Vertex, DistT);
-    std::vector<std::vector<DistT>> GetPath();
+    explicit GraphMatrix(VERTEX);
+    GraphMatrix(VERTEX, VERTEX, GraphList);
+    void AddEdge(VERTEX, VERTEX, DIST);
+    std::vector<std::vector<DIST>> GetPath();
 };
 
-Graph::Graph(Vertex num_vertex) {
+GraphMatrix::GraphMatrix(VERTEX num_vertex) {
     num_vertex_ = num_vertex;
     matrix_.resize(num_vertex_);
-    for (Vertex i = 0; i < num_vertex_; i++) {
+    for (VERTEX i = 0; i < num_vertex_; i++) {
         matrix_[i].resize(num_vertex_);
     }
 }
 
-void Graph::AddEdge(Vertex first, Vertex second, DistT weight) {
+GraphMatrix::GraphMatrix(VERTEX num_vertex, VERTEX num_edge, GraphList gl) {
+    num_vertex_ = num_vertex;
+    matrix_.resize(num_vertex_);
+    for (VERTEX i = 0; i < num_vertex_; i++) {
+        matrix_[i].resize(num_vertex_);
+    }
+    for (auto i = 0; i < num_edge; i++) {
+        matrix_[gl.list_[i].from][gl.list_[i].to] = gl.list_[i].weight;
+    }
+}
+
+void GraphMatrix::AddEdge(VERTEX first, VERTEX second, DIST weight) {
     matrix_[first][second] = weight;
 }
 
-void Graph::FloydWarshall() {
-    for (Vertex k = 0; k < num_vertex_; k++) {
-        for (Vertex x = 0; x < num_vertex_; x++) {
-            for (Vertex y = 0; y < num_vertex_; y++) {
+void GraphMatrix::FloydWarshall() {
+    for (VERTEX k = 0; k < num_vertex_; k++) {
+        for (VERTEX x = 0; x < num_vertex_; x++) {
+            for (VERTEX y = 0; y < num_vertex_; y++) {
                 matrix_[x][y] = std::min(matrix_[x][y], matrix_[x][k] + matrix_[k][y]);
             }
         }
     }
 }
 
-std::vector<std::vector<DistT>> Graph::GetPath() {
+std::vector<std::vector<DIST>> GraphMatrix::GetPath() {
     FloydWarshall();
     return matrix_;
 }
@@ -45,19 +89,19 @@ std::vector<std::vector<DistT>> Graph::GetPath() {
 int main() {
     std::cin.tie(nullptr);
     std::ios_base::sync_with_stdio(false);
-    Vertex num_vertex;
+    VERTEX num_vertex;
     std::cin >> num_vertex;
 
-    Graph g(num_vertex);
-    for (Vertex i = 0; i < num_vertex; i++) {
-        for (Vertex j = 0; j < num_vertex; j++) {
-            DistT weight = 0;
+    GraphMatrix g(num_vertex);
+    for (VERTEX i = 0; i < num_vertex; i++) {
+        for (VERTEX j = 0; j < num_vertex; j++) {
+            DIST weight = 0;
             std::cin >> weight;
             g.AddEdge(i, j, weight);
         }
     }
 
-    std::vector<std::vector<DistT>> new_path = g.GetPath();
+    std::vector<std::vector<DIST>> new_path = g.GetPath();
     for (auto& i : new_path) {
         for (auto& j : i) {
             std::cout << j << " ";
